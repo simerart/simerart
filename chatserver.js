2beb77c4af072845f4c20bafb6385a4ee7ad6757
simerart <robertsonsj@vcu.edu>
2024-12-16 23:58:04 -0500

    serverpt3

--- chatserver.js
+++ chatserver.js
@@ -1,7 +1,9 @@
 const io = require('socket.io')(3000)
 
 io.on('connection', socket => {
-    console.log('new user')
+
     socket.emit('chat-message', 'Hello World')
-    socket.on()
+    socket.on('send-chat-message', message => {
+        socket.broadcast.emit('chat-message', message)
+    })
 })
--- script.js
+++ script.js
@@ -1,14 +1,22 @@
 const socket = io('http//localhost:3000')
+const messageContainer = document.getElementById('message-container')
 const messageFrom = document.getElementById('send-container')
 const messageInput = document.getElementById('message-input')
 
 socket.on('chat-message', data => {
-    console.log(data)
+    appendMessage(data)
 })
 
 messageForm.addEventListener('submit', e => {
     e.preventDefault()
     const message = messageInput.Value
-    socket.emit('end-chaat-message', message)
+    appendMessage(`You: $(message)`)
+    socket.emit('send-chat-message', message)
     messageInput.value = ''
 })
+
+function appendMessage(message) {
+    const messageElement = document.createElement('div')
+    messageElement.innerText = message
+    messageContainer.append(messageElement)
+}
